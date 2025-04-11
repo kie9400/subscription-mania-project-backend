bfa37689d502b61115f0bcc62479c3c379cf9f87
kie9400 <sw8865@naver.com>
2025-04-11 10:57:34 +0900

    memberService 계층 구현, 회원가입 로직구현

--- src/main/java/com/springboot/member/service/MemberService.java
+++ src/main/java/com/springboot/member/service/MemberService.java
@@ -1,4 +1,63 @@
 package com.springboot.member.service;
 
+import com.springboot.auth.utils.AuthorityUtils;
+import com.springboot.exception.BusinessLogicException;
+import com.springboot.exception.ExceptionCode;
+import com.springboot.member.entity.Member;
+import com.springboot.member.repository.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthorityUtils authorityUtils;
+
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder, AuthorityUtils authorityUtils) {
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authorityUtils = authorityUtils;
+    }
+
+    public Member createMember(Member member){
+        verifyExistsEmail(member.getEmail());
+
+        String encryptedPassword = passwordEncoder.encode(member.getPassword());
+        member.setPassword(encryptedPassword);
+
+        List<String> roles = authorityUtils.createAuthorities(member.getEmail());
+        member.setRoles(roles);
+
+        return memberRepository.save(member);
+    }
+
+    //이메일이 중복인지 확인하는 메서드
+    public void verifyExistsEmail(String email){
+        Optional<Member> member = memberRepository.findByEmail(email);
+
+        if (member.isPresent())
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+    }
+
+    //사용자가 존재하는지 확인하는 메서드
+    public Member findVerifiedMember(long memberId){
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        Member member = optionalMember.orElseThrow(()->
+                new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return member;
+    }
+
+
+
+
+
 }
+
+
