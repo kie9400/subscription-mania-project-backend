45d433de13914baf8eff6f8d8a7c57f64e7f46cb
kie9400 <sw8865@naver.com>
2025-04-11 10:20:32 +0900

    member 임시 추가(spring security 구현을 위해 추가하였으며, 수정 필요)

---
+++ src/main/java/com/springboot/member/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.springboot.member.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/members")
+@Validated
+public class MemberController {
+
+}
+
---
+++ src/main/java/com/springboot/member/entity/Member.java
@@ -0,0 +1,73 @@
+package com.springboot.member.entity;
+
+import com.springboot.audit.BaseEntity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    private String image;
+
+    @Column(nullable = false, updatable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 13, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private int age;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private Gender gender;
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동 상태"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
+
+    public enum Gender {
+        MALE("남자"),
+        FEMALE("여자");
+
+        @Getter
+        private String gender;
+
+        Gender(String gender) {
+            this.gender = gender;
+        }
+    }
+}
