4f349d4c57ccdf7cb1c7d60befa0759c5157ec36
kie9400 <sw8865@naver.com>
2025-04-11 16:47:16 +0900

    실제 메일 전송을 위한 비즈니스 로직 구현

--- src/main/java/com/springboot/mail/service/MailService.java
+++ src/main/java/com/springboot/mail/service/MailService.java
@@ -1,4 +1,45 @@
 package com.springboot.mail.service;
 
+import com.springboot.exception.BusinessLogicException;
+import com.springboot.exception.ExceptionCode;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+
+@Transactional
+@Service
+@RequiredArgsConstructor
 public class MailService {
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+    private static final String TITLE = "[구독매니아] 이메일 인증 코드";
+
+    public void sendEmail(String email, String code) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        // Thymeleaf context 설정
+        Context context = new Context();
+        context.setVariable("code", code);
+
+        // 템플릿 렌더링
+        String htmlContent = templateEngine.process("email", context);
+
+        helper.setTo(email);
+        helper.setSubject(TITLE);
+        helper.setText(htmlContent, true);
+        helper.setReplyTo("subsMania-noreply@gmail.com"); //회신 불가능한 주소로 설정
+        try {
+            javaMailSender.send(message);
+        } catch (RuntimeException e) {
+            throw new BusinessLogicException(ExceptionCode.SEND_MAIL_FAILED);
+        }
+    }
 }
