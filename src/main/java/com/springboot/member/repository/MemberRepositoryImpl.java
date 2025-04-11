5d78754f96adb0a982bbf2287bfe586ca3740bef
hogeun <sw8865@naver.com>
2025-04-12 00:10:34 +0900

    실제 Member 쿼리를 작성하는 구현체 클래스 구현

--- src/main/java/com/springboot/member/repository/MemberRepositoryImpl.java
+++ src/main/java/com/springboot/member/repository/MemberRepositoryImpl.java
@@ -1,4 +1,29 @@
 package com.springboot.member.repository;
 
-public class MemberRepositoryImpl {
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.springboot.member.entity.Member;
+import com.springboot.member.entity.QMember;
+
+import java.util.Optional;
+
+//QueryDSL 구현 클래스
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    public MemberRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    //Optional<Member> findByEmail(String email) ->  queryDsl
+    @Override
+    public Optional<Member> findByEmailWithQuerydsl(String email) {
+        QMember member = QMember.member;
+
+        Member result = queryFactory
+                .selectFrom(member)
+                .where(member.email.eq(email))
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
 }
