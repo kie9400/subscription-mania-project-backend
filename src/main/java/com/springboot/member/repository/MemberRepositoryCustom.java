40c22eb64c11465ad58cf28290c16ce9dcedf99c
hogeun <sw8865@naver.com>
2025-04-12 00:09:09 +0900

    Member 사용자 정의 쿼리 메서드 인터페이스 구현

--- src/main/java/com/springboot/member/repository/MemberRepositoryCustom.java
+++ src/main/java/com/springboot/member/repository/MemberRepositoryCustom.java
@@ -1,4 +1,10 @@
 package com.springboot.member.repository;
 
+import com.springboot.member.entity.Member;
+
+import java.util.Optional;
+
 public interface MemberRepositoryCustom {
+    Optional<Member> findByEmailWithQuerydsl(String email);
 }
+
