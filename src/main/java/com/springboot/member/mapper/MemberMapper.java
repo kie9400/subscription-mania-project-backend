6f3c7876937081ea57f4466ac15488d1dd66701c
kie9400 <sw8865@naver.com>
2025-04-11 10:57:12 +0900

    member mapper 구현

--- src/main/java/com/springboot/member/mapper/MemberMapper.java
+++ src/main/java/com/springboot/member/mapper/MemberMapper.java
@@ -1,4 +1,13 @@
 package com.springboot.member.mapper;
 
+import com.springboot.member.dto.MemberDto;
+import com.springboot.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
+    Member memberPostToMember(MemberDto.Post requestBody);
 }
