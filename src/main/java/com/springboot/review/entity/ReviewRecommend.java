b365ec7506fce7701c5e4a1c6a69d3f093e54e49
hogeun <sw8865@naver.com>
2025-04-12 09:20:19 +0900

    Review 추천 entity 추가

---
+++ src/main/java/com/springboot/review/entity/ReviewRecommend.java
@@ -0,0 +1,26 @@
+package com.springboot.review.entity;
+
+import com.springboot.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReviewRecommend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recommendId;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
