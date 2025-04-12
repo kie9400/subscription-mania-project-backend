de833c1cf55f0662cafd88f6ca4162fb78a66aa7
hogeun <sw8865@naver.com>
2025-04-12 10:09:58 +0900

    구독플랜 entity 추가

---
+++ src/main/java/com/springboot/subscription/entity/SubsPlan.java
@@ -0,0 +1,36 @@
+package com.springboot.subscription.entity;
+
+import com.springboot.platform.entity.Platform;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class SubsPlan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long subsPlanId;
+
+    @Column(nullable = false)
+    private String planName;
+
+    @Column(nullable = false)
+    private int price;
+
+    @ManyToOne
+    @JoinColumn(name = "platform_id")
+    private Platform platform;
+
+    public void setPlatform(Platform platform) {
+        this.platform = platform;
+        if (!platform.getSubsPlans().contains(this)){
+            platform.setSubsPlan(this);
+        }
+    }
+}
