e4a2e51cdfed1b091eefde784b7734e470935de1
hogeun <sw8865@naver.com>
2025-04-12 23:56:30 +0900

    카테고리 데이터 insert하기 위한 sql파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (category_id, category_name, image)
+VALUES
+  (1, '문화', 'category/culture.png'),
+  (2, '도서', 'category/book.png'),
+  (3, '교육', 'category/study.png'),
+  (4, '음악', 'category/music.png'),
+  (5, '쇼핑', 'category/shopping.png'),
+  (6, '배달', 'category/delivery.png');
