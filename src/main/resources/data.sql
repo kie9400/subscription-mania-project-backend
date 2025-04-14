2a17a67925fa25a3ff5d7236d2eb6e162307f2c5
kie9400 <sw8865@naver.com>
2025-04-14 09:42:34 +0900

    더미데이터 쿼리 fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO category (category_id, category_name, image)
+INSERT INTO category (category_id, category_name, category_image)
 VALUES
-  (1, '문화', 'category/culture.png'),
-  (2, '도서', 'category/book.png'),
-  (3, '교육', 'category/study.png'),
-  (4, '음악', 'category/music.png'),
-  (5, '쇼핑', 'category/shopping.png'),
-  (6, '배달', 'category/delivery.png');
+  (1, '문화', 'images/category/culture.png'),
+  (2, '도서', 'images/category/book.png'),
+  (3, '교육', 'images/category/study.png'),
+  (4, '음악', 'images/category/music.png'),
+  (5, '쇼핑', 'images/category/shopping.png'),
+  (6, '배달', 'images/category/delivery.png');
