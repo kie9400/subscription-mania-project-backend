ca0ba937a29134b4c0649ac9a5c96ec6dc05d2aa
kie9400 <sw8865@naver.com>
2025-04-14 16:16:28 +0900

    플랫폼 카테고리-별점-정렬 조회 쿼리 구현 (검색시 , 미검색시 2개)

--- src/main/java/com/springboot/platform/repository/PlatformRepositoryCustom.java
+++ src/main/java/com/springboot/platform/repository/PlatformRepositoryCustom.java
@@ -5,5 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PlatformRepositoryCustom {
-    Page<Platform> findAllByCategory(Long categoryId, Pageable pageable);
+    //검색 포함 필터링
+    Page<Platform> findByCategoryAndKeywordAndRating
+            (Long categoryId, String keyword, Integer rating, Pageable pageable, String sort);
+
+    //검색 미포함 필터링
+    Page<Platform> findByCategoryAndRating(Long categoryId, Integer rating, Pageable pageable, String sort);
 }
--- src/main/java/com/springboot/platform/repository/PlatformRepositoryImpl.java
+++ src/main/java/com/springboot/platform/repository/PlatformRepositoryImpl.java
@@ -1,6 +1,9 @@
 package com.springboot.platform.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.springboot.category.entity.QCategory;
 import com.springboot.platform.entity.Platform;
@@ -22,16 +25,38 @@ public class PlatformRepositoryImpl implements PlatformRepositoryCustom{
     QCategory category = QCategory.category;
 
     @Override
-    public Page<Platform> findAllByCategory(Long categoryId, Pageable pageable) {
-        // 조건 처리 ( category를 선택하지 않았을경우 null 처리 ) -> 전체조회, 선택했으면 카테고리별로 조회
-        BooleanExpression condition = categoryId != null ? platform.category.categoryId.eq(categoryId) : null;
+    public Page<Platform> findByCategoryAndKeywordAndRating(Long categoryId, String keyword, Integer rating, Pageable pageable, String sort) {
+        OrderSpecifier<?> orderSpecifier = getOrderSpecifier(sort);
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (categoryId != null) {
+            builder.and(platform.category.categoryId.eq(categoryId));
+        }
+
+        if (keyword != null && !keyword.isBlank()) {
+            String processedKeyword = keyword.replace(" ", "");
+
+            //Expressions는 QueryDSL에서 SQL 함수를 직접 표현하고 싶을 때 사용한다.
+            //stringTemplate()메서드는 문자열 기반 SQL 함수 작성할때 사용
+            //밑에는 replace함수를 사용하여 데이터에 공백을 제거하고 비교한다.
+            builder.and(
+                    Expressions.stringTemplate("replace({0}, ' ', '')", platform.platformName)
+                            .containsIgnoreCase(processedKeyword)
+            );
+        }
+
+        if (rating != null) {
+            builder.and(platform.ratingAvg.goe(rating));
+            builder.and(platform.ratingAvg.lt(rating + 1));
+        }
 
         // 콘텐츠 조회
         // 조회된 데이터를 DTO의 생성자 순서대로 넣어서 반환한다.
         List<Platform> platforms = queryFactory
                 .selectFrom(platform)
                 .leftJoin(platform.category, category).fetchJoin()
-                .where(condition)
+                .where(builder)
+                .orderBy(orderSpecifier)
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
@@ -41,11 +66,57 @@ public class PlatformRepositoryImpl implements PlatformRepositoryCustom{
                 .select(platform.count())
                 .from(platform)
                 .leftJoin(platform.category, category)
-                .where(condition)
+                .where(builder)
                 //단일 결과 하나만 가져오기
                 .fetchOne();
 
         //total이 null이 아니면 그대로 쓰되 null이면 0
         return new PageImpl<>(platforms, pageable, total != null ? total : 0);
+    }
+
+    @Override
+    public Page<Platform> findByCategoryAndRating(Long categoryId, Integer rating, Pageable pageable, String sort) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (categoryId != null) {
+            builder.and(platform.category.categoryId.eq(categoryId));
+        }
+
+        if (rating != null) {
+            builder.and(platform.ratingAvg.goe(rating)); // 평균 별점 이상
+            builder.and(platform.ratingAvg.lt(rating + 1)); // 예: 3 이상 4 미만
+        }
+
+        OrderSpecifier<?> orderSpecifier = getOrderSpecifier(sort); // 정렬 기준
+
+        List<Platform> results = queryFactory
+                .selectFrom(platform)
+                .leftJoin(platform.category, category).fetchJoin()
+                .where(builder)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(orderSpecifier)
+                .fetch();
+
+        Long total = queryFactory
+                .select(platform.count())
+                .from(platform)
+                .leftJoin(platform.category, category)
+                .where(builder)
+                .fetchOne();
+
+        return new PageImpl<>(results, pageable, total != null ? total : 0);
+    }
+
+    //정렬 기준
+    private OrderSpecifier<?> getOrderSpecifier(String sort) {
+        switch (sort) {
+            case "rating":
+                return platform.ratingAvg.desc();
+            case "review":
+                return platform.reviewCount.desc();
+            default:
+                return platform.platformId.asc(); // 기본 정렬: 등록순
+        }
     }
 }
