7232ea1ee1474c0a4c45494fc4aaf3b6ebc87e38
kie9400 <sw8865@naver.com>
2025-04-14 09:44:28 +0900

    카테고리 mapper 구현

---
+++ src/main/java/com/springboot/category/mapper/CategoryMapper.java
@@ -0,0 +1,12 @@
+package com.springboot.category.mapper;
+
+import com.springboot.category.dto.CategoryDto;
+import com.springboot.category.entity.Category;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    List<CategoryDto.ResponseDto> categoryToCategoryResponseDtos(List<Category> categories);
+}
