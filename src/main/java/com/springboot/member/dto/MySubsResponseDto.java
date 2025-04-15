8d492ac49b520c76f32c60df5ce1f4be5ac92a5a
hogeun <sw8865@naver.com>
2025-04-15 22:37:54 +0900

    내 구독 내역 조회를 위한 MySubsResponseDto 구현

---
+++ src/main/java/com/springboot/member/dto/MySubsResponseDto.java
@@ -0,0 +1,43 @@
+package com.springboot.member.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MySubsResponseDto {
+    // 이번 달 총 요금
+    private int totalMonthlyPrice;
+
+    private List<CategoryGroup> categories;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class CategoryGroup {
+        private String categoryName;
+        private String categoryImage;
+        private int categoryTotalPrice;
+
+        private List<SubscriptionInfo> subscriptions;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class SubscriptionInfo {
+        private long subscriptionId;
+        private String platformImage;
+        private String platformName;
+        private String planName;
+        private int price;
+    }
+}
