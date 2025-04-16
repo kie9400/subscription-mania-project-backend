b754fde828c286f3a2d1dd21a89e1447b1982631
kie9400 <sw8865@naver.com>
2025-04-16 17:13:07 +0900

    결제일 갱신을 위한 현재 결제일 조회 쿼리 추가

--- src/main/java/com/springboot/subscription/repository/SubscriptionRepository.java
+++ src/main/java/com/springboot/subscription/repository/SubscriptionRepository.java
@@ -3,5 +3,9 @@ package com.springboot.subscription.repository;
 import com.springboot.subscription.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long>, SubscriptionRepositoryCustom {
+    List<Subscription> findAllByNextPaymentDate(LocalDate date);
 }
