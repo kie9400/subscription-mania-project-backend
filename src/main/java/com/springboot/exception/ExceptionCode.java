b4164c3f98e1f4566197b7401f4cf4e79a97fd47
kie9400 <sw8865@naver.com>
2025-04-29 13:54:31 +0900

    비밀번호 찾기 관련 예외 처리 로직 추가

--- src/main/java/com/springboot/exception/ExceptionCode.java
+++ src/main/java/com/springboot/exception/ExceptionCode.java
@@ -6,6 +6,8 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404,"회원을 찾을 수 없습니다."),
     MEMBER_EXISTS(409,"이미 가입된 회원입니다."),
     MEMBER_NOT_OWNER(403, "작성자만 가능합니다."),
+    MEMBER_NOT_NAME(404, "이름을 찾을 수 없습니다."),
+    MEMBER_NOT_PHONE(404, "휴대폰 번호를 찾을 수 없습니다."),
     MEMBER_NOT_ADMIN(403, "관리자 계정만 가능합니다."),
     MEMBER_DELETE(409, "이 회원은 탈퇴된 회원입니다."),
     PLATFORM_NOT_SUBSCRIPTION(403, "해당 플랫폼에 구독하지 않았습니다."),
