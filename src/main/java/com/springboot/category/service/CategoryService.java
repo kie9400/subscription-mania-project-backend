ec47ef29351f1040c9c37e87c7bbb4a3ac7c0fb8
kie9400 <sw8865@naver.com>
2025-04-30 10:17:00 +0900

    카테고리가 실제로 존재하는지 검증하는 메서드 추가

--- src/main/java/com/springboot/category/service/CategoryService.java
+++ src/main/java/com/springboot/category/service/CategoryService.java
@@ -2,10 +2,13 @@ package com.springboot.category.service;
 
 import com.springboot.category.entity.Category;
 import com.springboot.category.repository.CategoryRepository;
+import com.springboot.exception.BusinessLogicException;
+import com.springboot.exception.ExceptionCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -18,5 +21,14 @@ public class CategoryService {
 
     public List<Category> findCategories(){
         return categoryRepository.findAll();
+    }
+
+    public Category findVerifiedCategory(Long categoryId){
+        Optional<Category> optionalCategory = categoryRepository.findById(categoryId);
+
+        Category category =  optionalCategory.orElseThrow(()->
+                new BusinessLogicException(ExceptionCode.CATEGORY_NOT_FOUND));
+
+        return category;
     }
 }
